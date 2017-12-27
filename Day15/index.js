1cd26e7c7ea5be43c82f45962a92596ebb665a01
Abhishek Rathore <rathore.abhishek87@gmail.com>
2017-12-27 18:15:19 +0530

    day15

---
+++ Day15/index.js
@@ -0,0 +1,68 @@
+
+const express = require("express");
+const mongodb = require('mongodb');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const MongoClient = mongodb.MongoClient;
+const url = process.env.MONGODB_URI;
+
+app.use(cors())
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+mongoose.connect(url);
+var db = mongoose.connection;
+
+
+var userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+      },
+    age: {
+        type: Number,
+        required: true
+      }
+
+});
+
+var User = db.model('User',userSchema);
+
+
+
+app.post("/students",(req,res)=>{ 
+    var user = new User();
+    user.name = req.body.name;
+    user.age = req.body.age
+    user.save(function(err,doc){
+        res.json(doc);
+    });
+    
+})
+
+app.get("/students",(req,res)=>{ 
+    let name = req.query.user
+     db.collection('users').find({}).toArray(function(err,docs){
+         if(docs.length){
+             res.json(docs)
+         }else{
+             res.json({err:true,msg:"no user found"})
+         }
+     })
+ })
+
+ app.get("/deleteStudents",(req,res)=>{ 
+    let name = req.query.user
+     db.collection('students').deleteOne({"name":name},function(err,doc){
+         res.json(doc)
+     })
+ })
+
+app.post("/submit",(req,res)=>{
+    res.send("hi"+req.body.username+" your pass is "+req.body.pass );
+})
+
+app.listen(process.env.PORT,()=>{console.log("server started")})
